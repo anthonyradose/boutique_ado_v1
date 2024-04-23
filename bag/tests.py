374a50dcb80852c3fdfa373c3481c498fb057a82
Anthony Radose <asradose@gmail.com>
2024-04-23 16:41:48 +0000

    Add add to bag functionality

--- bag/tests.py
+++ bag/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+from django.urls import path
+from . import views
 
-# Create your tests here.
+
+urlpatterns = [
+    path('', views.view_bag, name='view_bag'),
+    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
+]
